6d07ee48442e434e80873697ba045cee57833482
gagapalola <sj0985109791@gmail.com>
2018-08-03 01:23:02 +0800

    電腦版已連結 CSS崩掉

---
+++ js/Modal_GetTic.js
@@ -0,0 +1,110 @@
+$("#movie_1").click(function () {
+    console.log(1)
+    $("#time_table1").addClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+
+$("#movie_2").click(function () {
+    console.log(2)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").addClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+
+$("#movie_3").click(function () {
+    console.log(3)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").addClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_4").click(function () {
+    console.log(4)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").addClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_5").click(function () {
+    console.log(5)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").addClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_6").click(function () {
+    console.log(6)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").addClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_7").click(function () {
+    console.log(7)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").addClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_8").click(function () {
+    console.log(8)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").addClass("active");
+    $("#time_table9").removeClass("active");
+});
+$("#movie_9").click(function () {
+    console.log(9)
+    $("#time_table1").removeClass("active");
+    $("#time_table2").removeClass("active");
+    $("#time_table3").removeClass("active");
+    $("#time_table4").removeClass("active");
+    $("#time_table5").removeClass("active");
+    $("#time_table6").removeClass("active");
+    $("#time_table7").removeClass("active");
+    $("#time_table8").removeClass("active");
+    $("#time_table9").addClass("active");
+});
