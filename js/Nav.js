120a444a59bd48e18c7a72ed6cde2c6c4124719a
gagapalola <sj0985109791@gmail.com>
2018-08-02 11:57:17 +0800

    NAV JS

---
+++ js/Nav.js
@@ -0,0 +1,5 @@
+    if ($(window).width() < 990) {
+        $(".navbar-brandImg").attr("src", "img/logo.png");
+    } else {
+        $(".navbar-brandImg").attr("src", "img/logo-Horizontal-white.png");
+    }
