12ed855443428856dd1616bbb057f929369dad19
gagapalola <sj0985109791@gmail.com>
2018-08-05 22:10:50 +0800

    RWD 修正

--- js/Nav.js
+++
@@ -1,5 +0,0 @@
-    if ($(window).width() < 990) {
-        $(".navbar-brandImg").attr("src", "img/logo.png");
-    } else {
-        $(".navbar-brandImg").attr("src", "img/logo-Horizontal-white.png");
-    }
